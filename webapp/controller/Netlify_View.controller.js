3b9b096a18ff3c78668a57f5e6577173a9e38a82
Manish <abap5788@gmail.com>
2021-08-22 20:34:16 +0530

    Update Netlify_View.controller.js

--- webapp/controller/Netlify_View.controller.js
+++ webapp/controller/Netlify_View.controller.js
@@ -6,16 +6,30 @@ sap.ui.define([
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller,MessageBox, MessageToast) {
-		"use strict";
+    function (Controller, MessageBox, MessageToast) {
+        "use strict";
 
-		return Controller.extend("pro.project1.controller.View", {
-			onInit: function () {
+        return Controller.extend("pro.project1.controller.View", {
+            onInit: function () {
 
             },
-            		handlePopoverPress: function (oEvent) {
-					MessageBox.information("Execute Order 66");
+            handlePopoverPress: function (oEvent) {
+                MessageBox.information("Execute Order 66");
 
-		}
-		});
-	});
+            },
+            onHomePress: function () {
+                var oIconTabHeader = this.byId('iconTabHeader');
+                oIconTabHeader.setSelectedKey('invalidKey');
+
+                var oLabel = this.byId('labelId');
+                oLabel.setText('Home Screen');
+            },
+
+            onSelectTab: function (event) {
+                var oLabel = this.byId('labelId');
+                var oTab = event.getParameter('item');
+
+                oLabel.setText(oTab.getText());
+            }
+        });
+    });
